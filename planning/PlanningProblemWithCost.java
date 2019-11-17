e016761d0325ff494edb6bf0345e8fd2f2564059
Max Lhermitte <max.lhermitte14@gmail.com>
2019-11-17 02:57:37 +0100

    fin Dijkstra

--- planning/PlanningProblemWithCost.java
+++ planning/PlanningProblemWithCost.java
@@ -9,34 +9,48 @@ import java.util.Set;
 import java.util.HashSet;
 
 public class PlanningProblemWithCost extends PlanningProblem{
-    
+    private Map<Action,Integer> cost;
 
-
-
-    protected State searchActionCostMin(Map<State,Integer> distance){
-
+    public PlanningProblemWithCost(State ini, State goal, List<Action> act, Map<Action,Integer> cost){
+        super(ini, goal, act);
+        this.cost = cost;
     }
 
-    public Stack<Action> Dijkstra(PlanningProblem problem){
+
+    protected static State searchStateDistanceMin(Map<State,Integer> distance, Set<State> open){
+        State state_smallest_distance = null;
+        int distanceMin = Integer.MAX_VALUE;
+        
+        for( State state_tested: open ){
+            if( distance.get(state_tested) < distanceMin ){
+                distanceMin = distance.get(state_tested);
+                state_smallest_distance = state_tested;
+            }
+        }
+
+        return state_smallest_distance;
+    }
+
+    public Stack<Action> Dijkstra(){
         Map<State,Integer> distance = new HashMap<State,Integer>();
         Map<State,State> father     = new HashMap<State,State>();
         Map<State,Action> plan      = new HashMap<State,Action>();
         Set<State> goals            = new HashSet<State>();
         Set<State> open             = new HashSet<State>();
         
-        open.add(problem.state_init);
-        distance.put(problem.state_init,0);
-        father.put(problem.state_init, null);
+        open.add(this.state_init);
+        distance.put(this.state_init,0);
+        father.put(this.state_init, null);
 
         while( !open.isEmpty() ){
-            State state = searchActionCostMin(open);
+            State state = searchStateDistanceMin(distance, open);
             open.remove(state);
 
-            if( state.satisfies(problem.state_goal) ){
+            if( state.satisfies(this.state_goal) ){
                 goals.add(state);
             }
 
-            for(Action act : problem.possible_actions){
+            for(Action act : this.possible_actions){
                 if( state.is_applicable(act) ){
                     State next = state.apply(act);
 
@@ -44,8 +58,8 @@ public class PlanningProblemWithCost extends PlanningProblem{
                         distance.put(next,Integer.MAX_VALUE);
                     }
 
-                    if( distance.get(next) > (distance.get(state) + act.cost) ){
-                        distance.get(next) = (distance.get(state) + act.cost);
+                    if( distance.get(next) > (distance.get(state) + cost.get(act)) ){
+                        distance.put(next,(distance.get(state) + cost.get(act)));
                         father.put(next,state);
                         plan.put(next,act);
                         open.add(next);
@@ -56,9 +70,9 @@ public class PlanningProblemWithCost extends PlanningProblem{
         return getDijkstraPlan(father, plan, goals, distance);
     }
 
-    protected Stack<Action> getDijkstraPlan(Map<State,State> father, Map<State,Action> actions, Set<State> goals, Map<State,Integer> distance){
+    protected static Stack<Action> getDijkstraPlan(Map<State,State> father, Map<State,Action> actions, Set<State> goals, Map<State,Integer> distance){
         Stack<Action> plan_d_action = new Stack<>();
-        goal = searchActionCostMin(open);
+        State goal = searchStateDistanceMin(distance, goals);
 
         while( !goals.isEmpty() ){
             plan_d_action.push(actions.get(goal) );
