66481eca3636199586e8507e74940a2aba6771db
Max Lhermitte <max.lhermitte14@gmail.com>
2019-11-18 12:00:37 +0100

    création Heuristic.java

---
+++ planning/Heurisitic.java
@@ -0,0 +1,13 @@
+package planning;
+
+import java.util.List;
+import java.util.HashMap;
+import java.util.Stack;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
+
+public class Heuristic{
+
+    
+}
