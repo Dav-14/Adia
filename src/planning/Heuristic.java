4e9f712f577b55708870a351a9095c273583da7f
Théo Dubus <theo.subud@gmail.com>
2019-11-19 17:13:04 +0100

    les switch c'est mieux

--- src/planning/Heuristic.java
+++ src/planning/Heuristic.java
@@ -7,21 +7,15 @@ public class Heuristic{
     public static int simpleHeuristic(State state){
         int eval = 0;
         
-        for(Variable var : state.keySet()){
-            String str = state.get(var);
-
-            if(str == "high"){
-                eval += 3;
-            }
-            if(str == "medium"){
-                eval += 2;
-            }
-            if(str == "low"){
-                eval += 1;
-            }
-            if(str == "null"){
-                eval += 0;
-            }
+        switch(str){
+            case "high":
+                heuristic += 3;
+            case "medium":
+                heuristic += 2;
+            case "low":
+                heuristic += 1;
+            case "null":
+                heuristic += 0;
         }
 
         return eval;
