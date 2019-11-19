a267f7489528e1b8acccf08c5ddea9c78777309d
tony_kero <antoine.karcher.id@gmail.com>
2019-11-19 14:31:12 +0100

    Added Main

---
+++ src/main/Main.java
@@ -0,0 +1,27 @@
+package main; 
+
+public class Main {
+
+    public static void execExamples(String[] args) {
+        examples.Main.main(args);
+    }
+    public static void execRepresentations(String[] args) {
+        representations.Main.main(args);
+    }
+    public static void execPPC(String[] args) {
+        ppc.Main.main(args);
+    }
+    public static void execDatamining(String[] args) {
+        datamining.Main.main(args);
+    }
+    public static void main(String[] args) {
+        System.out.println("-----PARTIE EXAMPLES-----");
+        execExamples(args);
+        System.out.println("--PARTIE REPRESENTATIONS-");
+        execRepresentations(args);
+        System.out.println("-------PARTIE PPC--------");
+        execPPC(args);
+        System.out.println("----PARTIE DATAMINING----");
+        execDatamining(args);
+    }
+}
